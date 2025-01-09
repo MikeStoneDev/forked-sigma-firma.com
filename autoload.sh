98080719b2e8409ce79a7cb57d26c55bba5c58f3
John Hartsfield <john@unilink.local>
2025-01-09 04:25:30 -0500

    updates

---
+++ autoload.sh
@@ -0,0 +1,7 @@
+# use in n/vim to restart on save:
+# :autocmd BufWritePost * silent! !./autoload.sh
+#!/bin/bash
+pkill boltapp || true
+go build -o boltapp
+echo http://localhost:10529
+./boltapp >> log.txt 2>&1 &
---
+++ internal/shared/head/head.js
@@ -0,0 +1,49 @@
+function jumpTo(eid) {
+    var jump = document.getElementById(eid);
+    jump.scrollIntoView({
+        behavior: 'auto',
+        block: 'center',
+        inline: 'center'
+    });
+}
+
+function toggleDisplay(elem) {
+    let divs = document.getElementById("hiddenTop").children;
+    let formDisplay = document.getElementById(elem);
+    for (let i=0;i<divs.length;i++) {
+        if (divs[i].id != formDisplay.id) {
+            divs[i].style.display = "none";
+        }
+    }
+    if (formDisplay.style.display == "none" || formDisplay.style.display == "") {
+        formDisplay.style.display = "unset";
+    } else {
+        formDisplay.style.display = "none";
+    }
+}
+
+async function getExample(view) {
+    const response = await fetch("/api/getExample", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({special:view}),
+    });
+
+    let res = await response.json();
+    if (res.success == "true") {
+        // do stuff
+    } else {
+        console.log("error");
+    }
+}
+
+window.onscroll = function() {
+    var a = document.getElementById("splash-inner");
+    var y = window.scrollY;
+    var f = (y/(window.innerHeight))
+    a.style.backdropFilter = "invert(" + f + ")";
+    a.style.filter = "invert(" + f + ")" + " sepia(" + f + ")" + "hue-rotate(" + (f+f)*300 + "deg)";
+    // if ((prcnt/(y+1)) < 0.3) {
+         
+    // }
+}
