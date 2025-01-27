d0e6adeeb75ea83d4a61d0709eb0872318929afd
John Hartsfield <john@unilink.local>
2025-01-26 21:20:08 -0500

    updates

---
+++ internal/components/term/term.js
@@ -0,0 +1,6 @@
+function delayClass() {
+    document.getElementById("term-outer").classList.add("term-hover");
+}
+
+// Execute myFunction after 1 second (1000 milliseconds)
+setTimeout(delayClass, 5000); 
