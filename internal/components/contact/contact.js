f270a5a1fb8560e499f6643a1e0133b48e544ea2
John Hartsfield <john@unilink.local>
2025-01-27 23:04:37 -0500

    updates

--- internal/components/contact/contact.js
+++ internal/components/contact/contact.js
@@ -1,54 +1,9 @@
 var cbutt = document.getElementById("contact-butt");
-async function postContact() {
-    if (!sent) {
-        var fd = document.getElementById("formData");
-        var formData = new FormData(fd);
-
-        if (validateFormData(formData)) {
-            var fd_s =     JSON.stringify(Object.fromEntries(formData));
-
-            cbutt.innerHTML = "sending...";
-            fd.style.filter = "blur(6px)";
-            const response = await fetch("/contact", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: fd_s,
-            });
-
-            let res = await response.json();
-            if (res.success == "true") {
-                var splash = document.getElementById("splash-inner");
-                document.getElementById("autosplash").style.opacity = "1";
-                document.getElementById("term-command").innerHTML = "welcome --to=destiny;";
-                document.getElementById("term-outer").style.position = "fixed";
-                // splash.style.backgroundImage = "url(public/media/hubble.jpg)";
-                // splash.style.backgroundSize = "contain";
-                // splash.style.zIndex = "-2";
-                // splash.style.filter = "unset";
-                // splash.style.opacity = "1";
-                splash.classList.add("splash-zoom");
-                var about = document.getElementById("about-outer");
-                about.innerHTML = "";
-                var contact = document.getElementById("contact-outer");
-                contact.innerHTML = "";
-                var foot = document.getElementById("footer-outer");
-                foot.innerHTML = "";
-                sent = true;
-                setTimeout(function() {location.reload()}, 5000);
-            } else {
-                fd.style.filter = "unset";
-            }
-        } else {
-            cbutt.classList.add("invalid-butt");
-        }
-    }
-}
 var form = document.getElementById("formData");
-var formData_ = new FormData(form);
+var formData = new FormData(form);
 form.addEventListener("input", function () {
     cbutt.innerHTML = "contact";
     cbutt.classList.remove("invalid-butt");
-
     for (let [key, value] of formData_.entries()) {
         switch (key) {
             case 'first_name':
@@ -66,9 +21,39 @@ form.addEventListener("input", function () {
         }
     }
 });
+async function postContact() {
+    if (!sent) {
+        if (validateFormData(formData)) {
+            var fd_s =     JSON.stringify(Object.fromEntries(formData));
+            cbutt.innerHTML = "sending...";
+            form.style.filter = "blur(6px)";
 
+            const response = await fetch("/contact", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: fd_s,
+            });
+
+            let res = await response.json();
+            if (res.success == "true") {
+                document.getElementById("splash-inner").classList.add("splash-zoom");
+                document.getElementById("about-outer").innerHTML = "";
+                document.getElementById("contact-outer").innerHTML = "";
+                document.getElementById("footer-outer").innerHTML = "";
+                document.getElementById("autosplash").style.opacity = "1";
+                document.getElementById("term-command").innerHTML = "welcome --to=destiny;";
+                document.getElementById("term-outer").style.position = "fixed";
+                sent = true;
+                setTimeout(function() {location.reload()}, 5000);
+            } else {
+                form.style.filter = "unset";
+            }
+        } else {
+            cbutt.classList.add("invalid-butt");
+        }
+    }
+}
 function validateFormData(formData) {
-    // Check if formData is an instance of FormData
     if (!(formData instanceof FormData)) {
         throw new Error('Invalid argument: formData must be an instance of FormData.');
     }
@@ -76,8 +61,6 @@ function validateFormData(formData) {
 
     // Iterate over the form data entries
     for (let [key, value] of formData.entries()) {
-
-        // Perform validation based on field name
         switch (key) {
             case 'first_name':
                 if (!inputinvalid(key, value)) {
@@ -98,7 +81,6 @@ function validateFormData(formData) {
                 }
                 break;
             case 'email':
-                console.log(key, value);
                 if (validateEmail(value)) {
                     isValid = true
                     break;
@@ -111,9 +93,7 @@ function validateFormData(formData) {
         }
     }
     return isValid;
-
 }
-
 function inputinvalid(key, value) {
     var inp = document.getElementById("contact_" +key);
     if (value.trim() === '') {
@@ -124,9 +104,7 @@ function inputinvalid(key, value) {
     inp.classList.remove("invalid");
     return true
 }
-
 function validateEmail(email) {
-    // Use a regular expression for email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
