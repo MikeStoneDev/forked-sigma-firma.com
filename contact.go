ca97e4241ffce121cef1aad4e489d7e3721dbe97
John Hartsfield <john@unilink.local>
2025-01-28 04:44:53 -0500

    updates

--- contact.go
+++ contact.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/sigma-firma/gmailAPI"
 	ohsheet "github.com/sigma-firma/googlesheetsapi"
@@ -12,6 +14,53 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	cf, err := marshalContact(r)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if cf.Email == "" || cf.FirstName == cf.LastName {
+		ajaxResponse(w, map[string]string{"success": "false", "error": "invalid form data"})
+		return
+	}
+
+	msg := &inboxer.Msg{
+		To:        "leadership@sigma-firma.com",
+		Subject:   "New Contact",
+		Body:      formatContactEmail(cf),
+		ImagePath: "",
+		MimeType:  "",
+	}
+	err = bobbyEmail(msg)
+	if err != nil {
+		log.Println(err)
+		ajaxResponse(w, map[string]string{"success": "false", "error": "invalid form data"})
+		return
+	}
+	sendConf(cf)
+
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		log.Println(err)
+	}
+	_, err = addRow([]interface{}{
+		cf.LastName,
+		cf.FirstName,
+		time.Now().UTC().In(loc).Format("Jan 02 2006 03:04:05 PM"),
+		cf.Email,
+		cf.Phone,
+		cf.NewsLetter,
+		cf.Questionnaire,
+		false,
+	})
+	if err != nil {
+		log.Println(err)
+		ajaxResponse(w, map[string]string{"success": "false", "error": "invalid form data"})
+		return
+	}
+	ajaxResponse(w, map[string]string{"success": "true"})
+}
 func sendConf(c *contactForm) {
 	msg := &inboxer.Msg{
 		To:      c.Email,
--- handlers.go
+++ handlers.go
@@ -1,60 +1,9 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"time"
-
-	"github.com/sigma-firma/inboxer"
 )
 
 func root(w http.ResponseWriter, r *http.Request) {
 	exeTmpl(w, r, nil, "main.html")
-}
-func contact(w http.ResponseWriter, r *http.Request) {
-	cf, err := marshalContact(r)
-	if err != nil {
-		log.Println(err)
-	}
-
-	if cf.Email == "" || cf.FirstName == cf.LastName {
-		ajaxResponse(w, map[string]string{"success": "false", "message": "invalid form data"})
-		return
-	}
-
-	msg := &inboxer.Msg{
-		To:        "leadership@sigma-firma.com",
-		Subject:   "New Contact",
-		Body:      formatContactEmail(cf),
-		ImagePath: "",
-		MimeType:  "",
-	}
-	err = bobbyEmail(msg)
-	if err != nil {
-		log.Println(err)
-		ajaxResponse(w, map[string]string{"success": "false", "message": "invalid form data"})
-		return
-	}
-	sendConf(cf)
-
-	loc, err := time.LoadLocation("America/New_York")
-	if err != nil {
-		log.Println(err)
-	}
-	_, err = addRow([]interface{}{
-		cf.LastName,
-		cf.FirstName,
-		time.Now().UTC().In(loc).Format("Jan 02 2006 03:04:05 PM"),
-		cf.Email,
-		cf.Phone,
-		cf.NewsLetter,
-		cf.Questionnaire,
-		false,
-	})
-	if err != nil {
-		log.Println(err)
-		ajaxResponse(w, map[string]string{"success": "false", "message": "invalid form data"})
-		return
-	}
-	ajaxResponse(w, map[string]string{"success": "true"})
 }
